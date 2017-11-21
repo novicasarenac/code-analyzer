a17f2a1431b2c1ea38ad41fe19b28b3dc9e1d9fe
novicasarenac <nolesarenac@yahoo.com>
2017-11-21 22:37:54 +0100

    neo4j properties added

--- src/main/java/codeAnalyzer/CodeAnalyzerApplication.java
+++ src/main/java/codeAnalyzer/CodeAnalyzerApplication.java
@@ -2,8 +2,10 @@ package codeAnalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("codeAnalyzer.model")
 public class CodeAnalyzerApplication {
 
 	public static void main(String[] args) {
